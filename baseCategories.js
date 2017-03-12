54358f34d77d546e9727b9b0201ca753b18128ec
Luis <anonimous357@gmail.com>
2017-03-12 14:14:43 -0600

    Add files for base

---
+++ baseCategories.js
@@ -0,0 +1,632 @@
+categories = [
+ {
+   "Id": 1,
+   "ParentId": null,
+   "Name": "Administración General",
+   "Slug": "admin",
+   "StyleName": 'administracion_general',
+   "ImageSource": 'fingerprint_administracion_general.png',
+   "ShowSlider": true
+ },
+ {
+   "Id": 2,
+   "ParentId": "1",
+   "Name": "Administración de recursos humanos",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 3,
+   "ParentId": "1",
+   "Name": "Administración Estratégica",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 4,
+   "ParentId": "1",
+   "Name": "Administración general",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 5,
+   "ParentId": "1",
+   "Name": "Contabilidad",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 6,
+   "ParentId": "1",
+   "Name": "Economía general",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 7,
+   "ParentId": "1",
+   "Name": "Ecuaciones diferenciales",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 8,
+   "ParentId": "1",
+   "Name": "Economía internacional",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 9,
+   "ParentId": "1",
+   "Name": "Finanzas",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 10,
+   "ParentId": "1",
+   "Name": "Macroeconomía",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 11,
+   "ParentId": "1",
+   "Name": "Marketing",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 12,
+   "ParentId": "1",
+   "Name": "Microeconomía",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 13,
+   "ParentId": "1",
+   "Name": "Producción",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 14,
+   "ParentId": "1",
+   "Name": "Recursos humanos",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 15,
+   "ParentId": "1",
+   "Name": "Econometría",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 16,
+   "ParentId": null,
+   "Name": "Ingeniería y computación",
+   "Slug": "ingen",
+   "StyleName": 'ingenieria_y_computacion',
+   "ImageSource": 'fingerprint_ingenieria_y_computacion.png',
+   "ShowSlider": true
+ },
+ {
+   "Id": 17,
+   "ParentId": "16",
+   "Name": "Ingeniería civil ",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 18,
+   "ParentId": "16",
+   "Name": "Ingeniería eléctrica",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 19,
+   "ParentId": "16",
+   "Name": "Ingeniería electrónica",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 20,
+   "ParentId": "16",
+   "Name": "Ingeniería general",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 21,
+   "ParentId": "16",
+   "Name": "Ingeniería industrial",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 22,
+   "ParentId": "16",
+   "Name": "Ingeniería mecánica",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 23,
+   "ParentId": "16",
+   "Name": "Ingeniería química",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 24,
+   "ParentId": "16",
+   "Name": "Base de datos",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 25,
+   "ParentId": "16",
+   "Name": "Compiladores",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 26,
+   "ParentId": "16",
+   "Name": "Diseño",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 27,
+   "ParentId": "16",
+   "Name": "Estructura de datos",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 28,
+   "ParentId": "16",
+   "Name": "Ingeniería de software",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 29,
+   "ParentId": "16",
+   "Name": "Introducción a la computación",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 30,
+   "ParentId": "16",
+   "Name": "Lenguajes y programación",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 31,
+   "ParentId": "16",
+   "Name": "Redes",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 32,
+   "ParentId": "16",
+   "Name": "Sistemas de información",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 33,
+   "ParentId": "16",
+   "Name": "Sistemas operativos",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 34,
+   "ParentId": "16",
+   "Name": "Software de aplicación",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 35,
+   "ParentId": null,
+   "Name": "Matemáticas y ciencias",
+   "Slug": "cienc",
+   "StyleName": 'ciencias_y_matematicas',
+   "ImageSource": 'fingerprint_ciencias_y_matematicas.png',
+   "ShowSlider": true
+ },
+ {
+   "Id": 36,
+   "ParentId": "35",
+   "Name": "Álgebra ",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 37,
+   "ParentId": "35",
+   "Name": "Análisis numérico",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 38,
+   "ParentId": "35",
+   "Name": "Cálculo",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 39,
+   "ParentId": "35",
+   "Name": "Ecuaciones diferenciales",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 40,
+   "ParentId": "35",
+   "Name": "Estadística",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 41,
+   "ParentId": "35",
+   "Name": "Estadística aplicada",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 42,
+   "ParentId": "35",
+   "Name": "Geometría",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 43,
+   "ParentId": "35",
+   "Name": "Introducción a las matemáticas",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 44,
+   "ParentId": "35",
+   "Name": "Investigación de operaciones",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 45,
+   "ParentId": "35",
+   "Name": "Matemática avanzada",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 46,
+   "ParentId": "35",
+   "Name": "Matemáticas aplicadas",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 47,
+   "ParentId": "35",
+   "Name": "Biología",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 48,
+   "ParentId": "35",
+   "Name": "Física",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 49,
+   "ParentId": "35",
+   "Name": "Geografía",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 50,
+   "ParentId": "35",
+   "Name": "Medicina",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 51,
+   "ParentId": "35",
+   "Name": "Química",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 52,
+   "ParentId": "35",
+   "Name": "Geología",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 53,
+   "ParentId": null,
+   "Name": "Ciencias sociales",
+   "Slug": "socia",
+   "StyleName": 'ciencias_sociales',
+   "ImageSource": 'fingerprint_ciencias_sociales.png',
+   "ShowSlider": true
+ },
+ {
+   "Id": 54,
+   "ParentId": "53",
+   "Name": "Ciencias políticas",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 55,
+   "ParentId": "53",
+   "Name": "Comunicación",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 56,
+   "ParentId": "53",
+   "Name": "Derecho ",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 57,
+   "ParentId": "53",
+   "Name": "Educación",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 58,
+   "ParentId": "53",
+   "Name": "Filosofía",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 59,
+   "ParentId": "53",
+   "Name": "Historia",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 60,
+   "ParentId": "53",
+   "Name": "Metodología de la investigación",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 61,
+   "ParentId": "53",
+   "Name": "Psicología",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 62,
+   "ParentId": "53",
+   "Name": "Sociología",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 63,
+   "ParentId": "53",
+   "Name": "Literatura",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 64,
+   "ParentId": null,
+   "Name": "Productos digitales",
+   "Slug": "digit",
+   "StyleName": 'productos_digitales',
+   "ImageSource": 'fingerprint_productos_digitales.png',
+   "ShowSlider": true
+ },
+ {
+   "Id": 65,
+   "ParentId": "64",
+   "Name": "MyMathLab Español",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 66,
+   "ParentId": "64",
+   "Name": "eText | español",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 67,
+   "ParentId": "64",
+   "Name": "MyLab | en español",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+   "Id": 68,
+   "ParentId": "64",
+   "Name": "Mastering Physics",
+   "Slug": null,
+   "StyleName": null,
+   "ImageSource": null,
+   "ShowSlider": false
+ },
+ {
+    "Id": 69,
+    "ParentId": "64",
+    "Name": "Pearson eText",
+    "Slug": null,
+    "StyleName": null,
+    "ImageSource": null,
+    "ShowSlider": false
+ },
+ {
+    "Id": 70,
+    "ParentId": null,
+    "Name": "Custom",
+    "Slug": "custo",
+    "StyleName": "custom",
+    "ImageSource": "fingerprint_custom.png",
+    "ShowSlider": true
+  }
+];
