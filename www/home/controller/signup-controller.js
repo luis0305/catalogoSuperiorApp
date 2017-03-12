366169f4a1cdbe5799c13605b20ac5af03bd1c15
Luis <anonimous357@gmail.com>
2017-03-12 11:42:59 -0600

    Add popup process signup

--- www/home/controller/signup-controller.js
+++ www/home/controller/signup-controller.js
@@ -1,10 +1,21 @@
 (function (){
   appSignup = angular.module('starter.signup.controller',[]);
 
-  appSignup.controller ('signup-controller', function ($scope, $state){
+  appSignup.controller ('signup-controller', function ($scope, $state, $ionicPopup, $timeout ){
     $scope.signUp = function (form){
-      if(form.$valid)
-        $state.go('home');
+      if(form.$valid){
+        var signupPopup = $ionicPopup.show({
+         templateUrl: 'home/views/popup-signup-view.html',
+         title: 'Registrando',
+         subTitle: 'Registrando...',
+         scope: $scope
+        });
+        $timeout(function() {
+          //add logic to register
+          signupPopup.close();
+          $state.go('home');
+        }, 3000);
+      }
     };
   });
 
