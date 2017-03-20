1065b8a6bedc932794fddc585533ff8f5a65633e
Luis <anonimous357@gmail.com>
2017-03-20 13:27:37 -0600

    Fix dynamic style

--- www/js/tabs-function.js
+++ www/js/tabs-function.js
@@ -11,10 +11,10 @@ function openInfo(evt, infoName) {
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" .tab-active", "");
+        tablinks[i].className = tablinks[i].className.replace(" tab-active", "");
     }
 
     // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(infoName).style.display = "block";
-    evt.currentTarget.className += " .tab-active";
+    evt.currentTarget.className += " tab-active";
 }
